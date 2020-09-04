06dfbc426a889a7dc2ba6c6e9812c70281263af0
afilipovski <69673676+afilipovski@users.noreply.github.com>
2020-09-04 16:47:05 +0200

    Added obstruction checking
    Figures no longer teleport without first considering whether the path they will have to cross is clear. This change finalizes all the general movement cases.
    
    TODO (in chronological order):
    - Checkmate, stalemate;
    - Pawn promotion;
    - En passant, castling.

--- Chess.h
+++ Chess.h
@@ -2,6 +2,12 @@
 #include<iostream>
 #include<string>
 #include<cmath>
+constexpr int sign_multiplier(int number) {
+	if (number >= 0)
+		return 1;
+	else
+		return -1;
+}
 class Chess {
 public:
 	friend std::ostream& print_board(std::ostream& os, const Chess& c1);
@@ -14,30 +20,40 @@ public:
 		int By = 7 - from_to[1] + '1';
 		int Dx = from_to[2] - 'a';
 		int Dy = 7 - from_to[3] + '1';
-		const char figura_tip = table[By][Bx];
 		bool black_turn = round_no % 2;
-		if ((!black_turn && islower(figura_tip)) || (black_turn && isupper(figura_tip))) {
-			std::cerr << "Incorrect turn.\n";
-			return 0;
-		}
 		if (Bx < 0 || Bx >  7 || By < 0 || By > 7 || Dx < 0 || Dx > 7 || Dy < 0 || Dy > 7 || (Bx == Dx && By == Dy)) {
-			std::cerr << "Fundamental rule broken.\n";
+			std::cout << "Fundamental rule broken.\n";
 			return 0;
 		}
-		if ((black_turn && islower(table[Dy][Dx])) || (!black_turn && isupper(table[Dy][Dx]))) {
-			std::cout << "Attacking own figure.\n";
+		if (isalpha(table[By][Bx]) && isalpha(table[Dy][Dx]) && isupper(table[By][Bx]) == isupper(table[Dy][Dx])) {
+			std::cout << "Occupied by own figure.\n";
 			return 0;
 		}
-		switch (figura_tip) {
+		switch (table[By][Bx]) {
 		case 'R':
 		case 'r':
-			if (!((Bx == Dx) || (By == Dy))) {
+			if (!(Bx == Dx || By == Dy)) {
 				std::cout << "Rook rule broken.\n";
 				return 0;
 			}
+			//rook path clearance check, generates a warning that the program might go out of bounds but that is an already rectified issue
+			//same goes for the bishop check
+			if (Bx == Dx)
+				for (int i = By + sign_multiplier(Dy-By); i != Dy; i += sign_multiplier(Dy - By))
+					if (table[i][Bx] != '-') {
+						std::cout << "Rook's path is obstructed.\n";
+						return 0;
+					}
+			if (By == Dy)
+				for (int i = Bx + sign_multiplier(Dx - Bx); i != Dx; i += sign_multiplier(Dx - Bx))
+					if (table[By][i] != '-') {
+						std::cout << "Rook's path is obstructed.\n";
+						return 0;
+					}
 			break;
 		case 'N':
 		case 'n':
+			//The knight "jumps over" to the destination, therefore no further movement restrictions are needed
 			if (!((abs(By - Dy) == 2 && abs(Bx - Dx) == 1) || (abs(Bx - Dx) == 2 && abs(By - Dy) == 1))) {
 				std::cout << "Knight rule broken.\n";
 				return 0;
@@ -46,33 +62,71 @@ public:
 		case 'B': 
 		case 'b':
 			if (!(Bx - Dx != 0 && abs(Bx - Dx) == abs(By - Dy))) {
-				std::cerr << "Bishop rule broken.\n";
+				std::cout << "Bishop rule broken.\n";
 				return 0;
+			}
+			{
+				int i = Bx + sign_multiplier(Dx - Bx);
+				int j = By + sign_multiplier(Dy - By);
+				while (i != Dx) {
+					if (table[j][i] != '-') {
+						std::cout << "Bishop's path is obstructed.\n";
+						return 0;
+					}
+					i += sign_multiplier(Dx - Bx); j += sign_multiplier(Dy - By);
+				}
 			}
 			break;
 		case 'Q':
 		case 'q':
-			if (!(((Bx == Dx) ^ (By == Dy)) || (Bx - Dx != 0 && abs(Bx - Dx) == abs(By - Dy)))) {
-				std::cerr << "Queen rule broken.\n";
+			if (!(Bx == Dx || By == Dy || abs(Bx - Dx) == abs(By - Dy))) {
+				std::cout << "Queen rule broken.\n";
 				return 0;
+			}
+			if (Bx == Dx) {
+				for (int i = By + sign_multiplier(Dy - By); i != Dy; i += sign_multiplier(Dy - By))
+					if (table[i][Bx] != '-') {
+						std::cout << "Queen-Rook's path is obstructed.\n";
+						return 0;
+					}
+			}
+			else if (By == Dy) {
+				for (int i = Bx + sign_multiplier(Dx - Bx); i != Dx; i += sign_multiplier(Dx - Bx))
+					if (table[By][i] != '-') {
+						std::cout << "Queen-Rook's path is obstructed.\n";
+						return 0;
+					}
+			}
+			else {
+				int i = Bx + sign_multiplier(Dx - Bx);
+				int j = By + sign_multiplier(Dy - By);
+				while (i != Dx) {
+					if (table[j][i] != '-') {
+						std::cout << "Queen-Bishop's path is obstructed.\n";
+						return 0;
+					}
+					i += sign_multiplier(Dx - Bx); j += sign_multiplier(Dy - By);
+				}
 			}
 			break;
 		case 'K':
 		case 'k':
 			if (!(abs(Bx - Dx) < 2 && abs(By - Dy) < 2)) {
-				std::cerr << "King rule broken.\n";
+				std::cout << "King rule broken.\n";
 				return 0;
 			}
 			break;
 		case 'P':
-			if (!(Bx == Dx && (Dy - By == -2 && By == 6 || Dy - By == -1) || (abs(Dx-Bx)==1 && Dy-By == -1 && islower(table[Dy][Dx])))) {
-				std::cout << "White pawn rule broken.\n";
+			if (!(Bx == Dx && table[Dy][Dx] == '-' && (Dy - By == -1 || Dy - By == -2 && table[Dy - 1][Dx] == '-' && By == 6) || 
+				(abs(Dx - Bx) == 1 && Dy - By == -1 && islower(table[Dy][Dx])))) {
+				std::cout << "White pawn rule broken or path obstructed.\n";
 				return 0;
 			}
 			break;
 		case 'p':
-			if (!(Bx == Dx && (Dy - By == 2 && By == 1 || Dy - By == 1) || (abs(Dx - Bx) == 1 && Dy - By == 1 && isupper(table[Dy][Dx])))) {
-				std::cout << "Black pawn rule broken.\n";
+			if (!(Bx == Dx && table[Dy][Dx] == '-' && (Dy - By == 1 || Dy - By == 2 && table[Dy - 1][Dx] == '-' && By == 1) ||
+				(abs(Dx - Bx) == 1 && Dy - By == -1 && isupper(table[Dy][Dx])))) {
+				std::cout << "Black pawn rule broken or path obstructed.\n";
 				return 0;
 			}
 			break;
